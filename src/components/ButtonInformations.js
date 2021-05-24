b524aca1024e3434f5c654ca51b50a3f26dcb5ff
Euclides Rodrigues Junior <61975704+Euclides-R@users.noreply.github.com>
2021-05-24 16:54:26 -0300

    Update src/components/ButtonInformations.js
    Co-authored-by: Euclécio Josias Rodrigues <euclecio.rodrigues@codenation.dev>

--- src/components/ButtonInformations.js
+++ src/components/ButtonInformations.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default props => (
+export default ButtonInformation({ link, title })  {
   <button class="link__information">
     <a href="#">{props.title}</a>
   </button>
