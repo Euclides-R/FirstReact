f51b0cfde785f63fceab5a5b61d5c0eb29c2ce14
Euclides-R <euclides.rodriguesjr@outlook.com>
2021-05-25 15:17:46 -0300

    Atualização de Footer

---
+++ src/pages/Details.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Details() {
+  return (
+    <div className="details">
+      <div className="content">
+        <div className="img-details">
+          <img src="/img/cuate.svg" />
+        </div>
+      </div>
+    </div>
+  );
+}
