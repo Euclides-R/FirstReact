23fe732ca6920cdc403205d4aedc363fc1780d73
Euclides-R <euclides.rodriguesjr@outlook.com>
2021-05-26 21:06:16 -0300

    Atualização de pages 🎯

---
+++ src/pages/Sign-in.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function SingIn() {
+  return (
+    <div className="singin">
+      <div className="content">
+        <div className="box-black">
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
