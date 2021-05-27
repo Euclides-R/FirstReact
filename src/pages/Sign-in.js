5f4987c43795163e91fad592e34ae8eac829bb66
Euclides-R <euclides.rodriguesjr@outlook.com>
2021-05-27 15:55:17 -0300

    Página de cadastro

--- src/pages/Sign-in.js
+++ src/pages/Sign-in.js
@@ -5,8 +5,63 @@ export default function SingIn() {
   return (
     <div className="singin">
       <div className="content">
-        <div className="box-black">
-          <div></div>
+        <div className="box-orange radius-form">
+          <div className="box-black radius-form">
+            <div className="img-signin">
+              <img src="/img/logo-cpa.png" />
+            </div>
+            <h1>Cadastre-se</h1>
+            <form className="form-signin">
+              <div className="form-box radius-form">
+                <p>Nome:</p>
+                <input
+                  type="text"
+                  name="nome"
+                  placeholder="Digite seu nome"
+                ></input>
+              </div>
+              <div className="form-box radius-form">
+                <p>Nome:</p>
+                <input
+                  type="text"
+                  name="nome"
+                  placeholder="Digite seu nome"
+                ></input>
+              </div>
+              <div className="form-box radius-form">
+                <p>Nome:</p>
+                <input
+                  type="text"
+                  name="nome"
+                  placeholder="Digite seu nome"
+                ></input>
+              </div>
+              <div className="form-box radius-form">
+                <p>Nome:</p>
+                <input
+                  type="text"
+                  name="nome"
+                  placeholder="Digite seu nome"
+                ></input>
+              </div>
+              <div className="form-box radius-form">
+                <p>Nome:</p>
+                <input
+                  type="text"
+                  name="nome"
+                  placeholder="Digite seu nome"
+                ></input>
+              </div>
+              <div className="form-box radius-form">
+                <p>Nome:</p>
+                <input
+                  type="text"
+                  name="nome"
+                  placeholder="Digite seu nome"
+                ></input>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
