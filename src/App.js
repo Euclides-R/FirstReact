7e1cecc81e27be15aab38ae8b05c512dc0f6e25f
Euclides-R <euclides.rodriguesjr@outlook.com>
2021-06-04 20:28:59 -0300

    Atualização de inputs, melhores nomes, e retirando códigos desnecessários

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import { Home, Details, SingIn, SingUp } from './pages';
+import { Home, Details, SingUp } from './pages';
 import { DefaultLayout } from './layouts';
 
 export default function App() {
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 
 export default function Input({ 
-    children,
-    kind,
-    className,
     typeInput, 
     placeHolder
 }) {
     return (
         <input 
-        className={classnames(`input input-${kind}`, className)}
+        className="insert--registration"
         type={typeInput}
         placeholder={placeHolder}
         >
-            {children}
         </input>
     )  
 }
