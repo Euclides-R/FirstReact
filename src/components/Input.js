752fc763b657c0413ec3c5ede921055a8ec41d2f
Euclides-R <euclides.rodriguesjr@outlook.com>
2021-06-11 20:21:14 -0300

    Atualização de códigos inputs e refatoração de código 🌐

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Input({ 
-    type, 
-    placeHolder
-}) {
+export default function Input({ type, placeHolder }) {
     return (
         <input 
         className="insert--registration"
