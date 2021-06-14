5352929023a7555778db4ad7d556e08a3a490ef5
Euclides-R <euclides.rodriguesjr@outlook.com>
2021-06-14 15:33:11 -0300

    Nova branch, para página de download

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from './Home';
 export { default as Details } from './Details';
 export { default as SingUp } from './SignUp';
+export { default as Downloadapp } from './Downloadapp';
